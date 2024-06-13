672ff70182fd8b9980b12aa1394524561ecb1c5c
Didier <did35@yahoo.com>
2024-06-13 08:43:16 +0200

    Initial Commit

---
+++ GeometryReader/GeometryReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryReaderApp.swift
+//  GeometryReader
+//
+//  Created by Didier Delhaisse on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct GeometryReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
